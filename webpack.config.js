c4b1ae4b79b99ddb5e8b25f35a926357a4d1e638
Morten Birk <mortenhbirk@gmail.com>
2020-01-30 16:49:40 +0100

    Update webpack loaders to ignore node_modules below fishfingers

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       rules: [
          {
             test: /\.jsx?$/,
-            //exclude: /node_modules/,
+            exclude: /node_modules\/fishfingers\/node_modules/,
             use: {
                loader: 'babel-loader',
                options: {
